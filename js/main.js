f89aa71c942377f50a70015337f9862f17aafefd
boy1434@naver.com <boy1434@naver.com>
2024-03-20 20:41:22 +0900

    구글 지도 마커 찍기

--- js/main.js
+++ js/main.js
@@ -1,19 +1,51 @@
 const API_KEY = "GWdZRWB1svhuRRxeQ3bBMiwlBqAQrkKTw%2BoEd0Zbjxxpaw9mykRs0NgETMIThRx9YOlYH33oHz1QkZm9GvfP%2FQ%3D%3D"
 
+// 공공데이터 api 가져오기
 const getData = async() => {
     const url = `http://apis.data.go.kr/6260000/FoodService/getFoodKr?ServiceKey=${API_KEY}&resultType=json&numOfRows=7&pageNo=1`;
     const response = await fetch(url);
     const data = await response.json();
     console.log("data", data);
+    const locations = data.getFoodKr.item.map((spot) =>[
+        spot.MAIN_TITLE,spot.LAT, spot.LNG
+    ]);
+    console.log("locations",locations);
+
+    drawMap(locations);
 }
 
 // 구글 지도 가져오기
-window.initMap = function () {
-    new google.maps.Map(document.getElementById("map"), {
-        center: {lat:35.2100142 , lng:129.0688702},
-        zoom: 10
+function drawMap(locations) {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 13,
+      center: new google.maps.LatLng(locations[0][1], locations[0][2]),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
     });
-}
+  
+    const infowindow = new google.maps.InfoWindow();
+  
+    let marker, i;
+  
+    for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map,
+      });
+  
+      google.maps.event.addListener(
+        marker,
+        "click",
+        (function (marker, i) {
+          return function () {
+            infowindow.setContent(locations[i][0]);
+            infowindow.open(map, marker);
+          };
+        })(marker, i)
+      );
+    }
+  }
+
+
 
 getData();
 
